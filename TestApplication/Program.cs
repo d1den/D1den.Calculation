56d834a39537f8196e724dcc5ee6ebd4af4e2bc2
Dmitry <denisoff.dan2013@yandex.ru>
2021-08-21 19:08:29 +0300

    Test first version

--- TestApplication/Program.cs
+++ TestApplication/Program.cs
@@ -11,6 +11,11 @@ namespace TestApplication
     {
         static void Main(string[] args)
         {
+            Point3D point1 = new Point3D();
+            Point3D point2 = new Point3D(2, 3.45, 2.14);
+            Point3D point3 = new Point3D(new double[] { 1.234, 124, -0.45 });
+            Console.WriteLine(point3.GetDistance(point2));
+            Console.WriteLine(Point3D.GetDistance(point1, point1));
         }
     }
 }
