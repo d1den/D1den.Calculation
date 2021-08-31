d1acbd44bd04ae96596d275cb8f1d6e958025606
Dmitry <denisoff.dan2013@yandex.ru>
2021-08-31 15:58:39 +0300

    Add convert methods in EulerAngles

--- MathLibraryProject/EulerAngles.cs
+++ MathLibraryProject/EulerAngles.cs
@@ -46,6 +46,8 @@ namespace d1den.MathLibrary
         /// </summary>
         public double Gamma { get { return _gamma; } }
 
+        public AngleUnits AngleUnits { get { return _angleUnits; } }
+
 
         public EulerAngles(double alpha, double betta, double gamma)
         {
@@ -84,6 +86,159 @@ namespace d1den.MathLibrary
         public double[] ToArray()
         {
             return new double[] { _alpha, _betta, _gamma };
+        }
+
+        public static EulerAngles GetEulersZXZFromRotation(Matrix rotationMatrix)
+        {
+            if (rotationMatrix.RowCount != 3 && rotationMatrix.ColumnCount != 3)
+            {
+                var ex = new ArgumentException("Matrix isn`t rotation, because dimencions are wrong!");
+                ex.Data.Add("Matrix dimensions", string.Format("({0},{1})",
+                    rotationMatrix.RowCount, rotationMatrix.ColumnCount));
+                throw ex;
+            }
+            else if (rotationMatrix[2,2] == 1.0)
+            {
+                double betta = 0;
+                double gamma = 0;
+                double alpha = Math.Atan2(rotationMatrix[1, 0], rotationMatrix[0, 0]);
+                return new EulerAngles(alpha, betta, gamma, AngleUnits.Radians);
+            }
+            else if(rotationMatrix[2, 2] == -1.0)
+            {
+                double betta = Math.PI;
+                double gamma = 0;
+                double alpha = Math.Atan2(rotationMatrix[1, 0], rotationMatrix[0, 0]);
+                return new EulerAngles(alpha, betta, gamma, AngleUnits.Radians);
+            }
+            else
+            {
+                double betta = Math.Atan2(Math.Sqrt(1.0 - Math.Pow(rotationMatrix[2, 2], 2.0)), rotationMatrix[2, 2]);
+                double gamma = Math.Atan2(rotationMatrix[2, 0], rotationMatrix[2, 1]);
+                double alpha = Math.Atan2(rotationMatrix[0, 2], -rotationMatrix[1, 2]);
+                return new EulerAngles(alpha, betta, gamma, AngleUnits.Radians);
+            }
+        }
+
+
+
+        public static EulerAngles GetEulersXYZFromRotation(Matrix rotationMatrix)
+        {
+            if (rotationMatrix.RowCount != 3 && rotationMatrix.ColumnCount != 3)
+            {
+                var ex = new ArgumentException("Matrix isn`t rotation, because dimencions are wrong!");
+                ex.Data.Add("Matrix dimensions", string.Format("({0},{1})",
+                    rotationMatrix.RowCount, rotationMatrix.ColumnCount));
+                throw ex;
+            }
+            else if (rotationMatrix[0, 2] == 1.0)
+            {
+                double betta = Math.PI / 2.0;
+                double gamma = 0;
+                double alpha = Math.Atan2(rotationMatrix[1, 0], rotationMatrix[1, 1]);
+                return new EulerAngles(alpha, betta, gamma, AngleUnits.Radians);
+            }
+            else if (rotationMatrix[0, 2] == -1.0)
+            {
+                double betta = -Math.PI / 2.0;
+                double gamma = 0;
+                double alpha = Math.Atan2(-rotationMatrix[1, 0], rotationMatrix[1, 1]);
+                return new EulerAngles(alpha, betta, gamma, AngleUnits.Radians);
+            }
+            else
+            {
+                double betta = Math.Atan2(rotationMatrix[0, 2], Math.Sqrt(1.0 - Math.Pow(rotationMatrix[0, 2], 2.0)));
+                double alpha = Math.Atan2(-rotationMatrix[1, 2], rotationMatrix[2, 2]);
+                double gamma = Math.Atan2(-rotationMatrix[0, 1], rotationMatrix[0, 0]);
+                return new EulerAngles(alpha, betta, gamma, AngleUnits.Radians);
+            }
+        }
+
+        public Matrix GetRotationFromZXZ()
+        {
+            EulerAngles eulers = this;
+            if (_angleUnits == AngleUnits.Degrees)
+            { 
+                eulers = ConvertToRadians();
+            }
+            double r11 = Math.Cos(eulers.Alpha) * Math.Sin(eulers.Gamma) - Math.Cos(eulers.Betta) * Math.Sin(eulers.Alpha) * Math.Sin(eulers.Gamma);
+            double r12 = -Math.Cos(eulers.Gamma)*Math.Sin(eulers.Alpha) -  Math.Cos(eulers.Alpha) * Math.Cos(eulers.Betta) * Math.Sin(eulers.Gamma);
+            double r13 = Math.Sin(eulers.Betta) * Math.Sin(eulers.Gamma);
+            double r21 = Math.Cos(eulers.Betta) * Math.Cos(eulers.Gamma) * Math.Sin(eulers.Alpha) + Math.Cos(eulers.Alpha) * Math.Sin(eulers.Gamma);
+            double r22 = Math.Cos(eulers.Alpha) * Math.Cos(eulers.Betta) * Math.Cos(eulers.Gamma) - Math.Sin(eulers.Alpha) * Math.Sin(eulers.Gamma);
+            double r23 = -Math.Cos(eulers.Gamma) * Math.Sin(eulers.Betta);
+            double r31 = Math.Sin(eulers.Alpha) * Math.Sin(eulers.Betta);
+            double r32 = Math.Cos(eulers.Alpha) * Math.Sin(eulers.Betta);
+            double r33 = Math.Cos(eulers.Betta);
+            return new Matrix(new[,]
+            {
+                { r11, r12, r13 },
+                { r21, r22, r23 },
+                { r31, r32, r33 }
+            });
+        }
+
+
+
+        public Matrix GetRotationFromXYZ()
+        {
+            EulerAngles eulers = this;
+            if (_angleUnits == AngleUnits.Degrees)
+            {
+                eulers = ConvertToRadians();
+            }
+            double r11 = Math.Cos(eulers.Betta) * Math.Cos(eulers.Gamma);
+            double r12 = -Math.Cos(eulers.Betta) * Math.Sin(eulers.Gamma);
+            double r13 = Math.Sin(eulers.Betta);
+            double r21 = Math.Cos(eulers.Alpha) * Math.Sin(eulers.Gamma) + Math.Cos(eulers.Gamma) * Math.Sin(eulers.Alpha) * Math.Sin(eulers.Betta);
+            double r22 = Math.Cos(eulers.Alpha) * Math.Cos(eulers.Gamma) - Math.Sin(eulers.Alpha) * Math.Sin(eulers.Betta) * Math.Sin(eulers.Gamma);
+            double r23 = -Math.Cos(eulers.Betta) * Math.Sin(eulers.Alpha);
+            double r31 = Math.Sin(eulers.Alpha) * Math.Sin(eulers.Gamma) - Math.Cos(eulers.Alpha) * Math.Cos(eulers.Gamma) * Math.Sin(eulers.Betta);
+            double r32 = Math.Cos(eulers.Gamma) * Math.Sin(eulers.Alpha) + Math.Cos(eulers.Alpha) * Math.Sin(eulers.Betta) * Math.Sin(eulers.Gamma);
+            double r33 = Math.Cos(eulers.Alpha) * Math.Cos(eulers.Betta);
+            return new Matrix(new[,]
+            {
+                { r11, r12, r13 },
+                { r21, r22, r23 },
+                { r31, r32, r33 }
+            });
+        }
+
+        public EulerAngles ConvertToRadians()
+        {
+            if (_angleUnits == AngleUnits.Degrees)
+                return new EulerAngles(AdvancedMath.DegreeToRadian(_alpha),
+                    AdvancedMath.DegreeToRadian(_betta),
+                    AdvancedMath.DegreeToRadian(_gamma),
+                    AngleUnits.Radians);
+            else
+                return this;
+        }
+
+
+        public EulerAngles ConvertToDegrees()
+        {
+            if (_angleUnits == AngleUnits.Radians)
+                return new EulerAngles(AdvancedMath.RadianToDegree(_alpha),
+                    AdvancedMath.RadianToDegree(_betta),
+                    AdvancedMath.RadianToDegree(_gamma));
+            else
+                return this;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("A={0:F2}, B={1:F2}, G={2:F2}", _alpha, _betta, _gamma);
+        }
+
+        public string ToString(AngleUnits angleUnits)
+        {
+            if (angleUnits == _angleUnits)
+                return ToString();
+            else if (angleUnits == AngleUnits.Radians)
+                return ConvertToDegrees().ToString();
+            else
+                return ConvertToRadians().ToString();
         }
     }
 }
