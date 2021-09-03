7702ae047776cd2278ed50a3ef7c446e19e2c99f
Dmitry <denisoff.dan2013@yandex.ru>
2021-09-03 10:06:25 +0300

    matrix tests done

--- TestForLibrary/MatrixImmutabilityTests.cs
+++ TestForLibrary/MatrixImmutabilityTests.cs
@@ -136,5 +136,77 @@ namespace TestForLibrary
                 matrix[1, 0] == 0 && matrix[1, 1] == 5)
                 Assert.Pass();
         }
+        [Test]
+        public void MultiplyMatriсes()
+        {
+            Matrix matrix = new[,]
+            {
+                {1,2 },
+                {3,4 }
+            };
+            Matrix matrix2 = Matrix.GetEyeMatrix(2);
+            Matrix matrix3 = matrix.Multiply(matrix2);
+            if (matrix[0, 0] == 1 && matrix[0, 1] == 2 &&
+                matrix[1, 0] == 3 && matrix[1, 1] == 4)
+                Assert.Pass();
+        }
+        [Test]
+        public void Invert()
+        {
+            Matrix matrix = new[,]
+            {
+                {2,2 },
+                {7,6 }
+            };
+            Matrix matrixInvert = matrix.Invert();
+            if (matrix[0, 0] == 2 && matrix[0, 1] == 2 &&
+                matrix[1, 0] == 7 && matrix[1, 1] == 6)
+                Assert.Pass();
+        }
+        [Test]
+        public void CreateMatrixAndChangeBaseArray()
+        {
+            var array1 = new int[,] { { 1, 2 }, { 3, 4 } };
+            var matrix1 = new Matrix(array1);
+            array1[0, 0] = -10;
+            var array2 = new double[,] { { 1.1, 2.2 }, { 3.3, 4.4 } };
+            var matrix2 = new Matrix(array2);
+            array2[0, 0] = -10;
+            if (matrix1[0,0] != -10 && matrix2[0,0] != -10)
+                Assert.Pass();
+        }
+        [Test]
+        public void ChangeMatrixData()
+        {
+            var array1 = new int[,] { { 1, 2 }, { 3, 4 } };
+            var matrix1 = new Matrix(array1);
+            double[,] matrixDataDouble = matrix1.MatrixData;
+            matrixDataDouble[0, 0] = -10;
+            int[,] matrixDataInt = matrix1.Int32MatrixData;
+            matrixDataInt[0, 0] = -10;
+            if (matrix1[0,0] != -10)
+                Assert.Pass();
+        }
+        [Test]
+        public void SetAllValues()
+        {
+            var array1 = new int[,] { { 1, 2 }, { 3, 4 } };
+            var matrix1 = new Matrix(array1);
+            Matrix matrix2 = matrix1.SetAllValues(-10);
+            if (matrix1[0, 0] != -10)
+                Assert.Pass();
+        }
+        [Test]
+        public void CopyAndCloneChange()
+        {
+            var array1 = new int[,] { { 1, 2 }, { 3, 4 } };
+            var matrix1 = new Matrix(array1);
+            Matrix matrix2 = matrix1;
+            matrix2.MatrixData[0, 0] = -10;
+            Matrix matrix3 = matrix1.Clone() as Matrix;
+            matrix3.MatrixData[0, 0] = -10;
+            if (matrix1[0, 0] != -10)
+                Assert.Pass();
+        }
     }
 }
